86e65186dcb5d77d32d1de7bb24181f0403935e0
Aman Mittal <amandeepmittal@live.com>
2022-05-06 13:32:12 +0530

    create example app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import { tx } from '@transifex/native';
+import { T, LanguagePicker, useLocale } from '@transifex/react';
+
 import './App.css';
 
+tx.init({
+  token: process.env.REACT_APP_TRANSIFEX_TOKEN,
+});
+
 function App() {
+  const currentLocale = useLocale();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    alert('Your form is submitted!');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="app">
+      <div className="form">
+        <h1>
+          <T _str="Login form" />
+        </h1>
+        <p className="subtitle">
+          <T _str="Please enter your credentials to proceed." />
+          <p>Currently selected locale is {currentLocale}.</p>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="picker">
+          <p className="picker-title">
+            <T _str="Select the language: " />
+          </p>{' '}
+          <LanguagePicker />
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <div className="input-container">
+            <label>
+              <T _str="Email" />
+            </label>
+            <input type="text" name="email" required />
+          </div>
+          <div className="input-container">
+            <label>
+              <T _str="Password" />
+            </label>
+            <input type="password" name="password" required />
+          </div>
+          <button className="button-container" type="submit">
+            <p className="button-text">
+              <T _str="Sign in" />
+            </p>
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
